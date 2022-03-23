501c083d3ada2c3f7698a61939a37deb4c6338de
mrgulugulu <492238938@qq.com>
2022-03-23 01:14:44 -0700

    final version

--- server/main.go
+++ server/main.go
@@ -62,10 +62,10 @@ func (manager *ClientManager) start() {
 			manager.clients[conn.id] = conn
 			//把返回连接成功的消息json格式化
 			jsonMessage, _ := json.Marshal(&Message{Content: "A new socket has connected. ", ServerIP: LocalIp(), SenderIP: conn.socket.RemoteAddr().String()})
-			manager.send(jsonMessage)
+			//manager.send(jsonMessage)
 
 			//同步生产消息模式
-			// syncProducer(jsonMessage)
+			syncProducer(jsonMessage)
 			//如果连接断开了
 		case conn := <-manager.unregister:
 			//判断连接的状态，如果是true,就关闭send，删除连接client的值
@@ -73,8 +73,8 @@ func (manager *ClientManager) start() {
 				close(conn.send)
 				delete(manager.clients, conn.id)
 				jsonMessage, _ := json.Marshal(&Message{Content: "A socket has disconnected. ", ServerIP: conn.socket.LocalAddr().String(), SenderIP: conn.socket.RemoteAddr().String()})
-				manager.send(jsonMessage)
-				// syncProducer(jsonMessage)
+				//manager.send(jsonMessage)
+				syncProducer(jsonMessage)
 			}
 			//广播
 		case message := <-manager.broadcast:
@@ -127,9 +127,9 @@ func (c *Client) read() {
 		// 序列化message
 		jsonMessage, _ := json.Marshal(&message)
 		fmt.Println(fmt.Sprintf("read Id:%s, msg:%s", c.id, string(jsonMessage)))
-		manager.broadcast <- jsonMessage
+		//manager.broadcast <- jsonMessage
 		// 放入kafka
-		// syncProducer(jsonMessage)
+		syncProducer(jsonMessage)
 	}
 }
 
@@ -159,13 +159,13 @@ func main() {
 	fmt.Println("Starting application...")
 	//开一个goroutine执行开始程序
 	go manager.start()
-	// initial()
+	initial()
 	//注册默认路由为 /ws ，并使用wsHandler这个方法
 	http.HandleFunc("/ws", wsHandler)
 	http.HandleFunc("/health", healthHandler)
 	//监听本地的8080端口
 	fmt.Println("chat server start.....")
-	_ = http.ListenAndServe(":8080", nil)
+	_ = http.ListenAndServe("localhost:8080", nil)
 }
 
 func wsHandler(res http.ResponseWriter, req *http.Request) {
@@ -179,6 +179,7 @@ func wsHandler(res http.ResponseWriter, req *http.Request) {
 	//每一次连接都会新开一个client，client.id通过uuid生成保证每次都是不同的
 	client := &Client{id: uuid.Must(uuid.NewV4(), nil).String(), socket: conn, send: make(chan []byte)}
 	fmt.Println(LocalIp())
+
 	//注册一个新的链接
 	manager.register <- client
 
@@ -219,7 +220,7 @@ func initial() {
 	config.Consumer.Return.Errors = true
 	fmt.Println("start connect kafka")
 	// 开始连接kafka服务器
-	address := []string{"127.0.0.1:9092"}
+	address := []string{"10.101.110.38:9092"}
 	client, err := sarama.NewClient(address, config)
 	if err != nil {
 		fmt.Println("connect kafka failed; err", err)
